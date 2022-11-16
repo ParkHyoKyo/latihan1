929dd6f425fb118b35ec40ffe0ad8b4ee7c45d42
ParkHyoKyo <budiavizone@gmail.com>
2022-11-16 22:28:19 +0700

    membuat inputan

--- latihan.php
+++ latihan.php
@@ -15,11 +15,17 @@
 
 // var_dump($Ohayo); //fungsi untuk mengetahui jenis variable atau tipe data
 
-$Ohayo = ["andi",1,"tanya"];
+// $Ohayo = ["andi",1,"tanya"];
 // var_dump($Ohayo)
 
-foreach ($Ohayo as $Arigato) {
-	echo $Arigato;
+// foreach ($Ohayo as $Arigato) {
+// 	echo $Arigato;
+// }
+
+$ohayo = ["ohayo" , "gozaimasu" , "gongxi" , "fakcai"];
+
+foreach ($ohayo as $ohayo) {
+	echo $ohayo. "<br>";
 }
 
 ?>
---
+++ proses.php
@@ -0,0 +1,11 @@
+<?php 
+
+$ohayo = $_GET['nama_depan'];
+$ohayogozaimasu = $_GET['nama_belakang'];
+
+echo $ohayo."<br>";
+echo $ohayogozaimasu;
+
+
+
+ ?>
